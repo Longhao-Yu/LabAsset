bcbe443700ddcc9ae406332c4b13858539130be4
Longhao-Yu <70065549+Longhao-Yu@users.noreply.github.com>
2020-08-23 05:53:49 +1000

    80 percent complete

---
+++ Lab Week 4 - Activity/Assets/ConsolePrint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsolePrint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < 1000; i++)
+        {
+            Debug.Log(gameObject.name + i);
+        }
+
+    }
+}
